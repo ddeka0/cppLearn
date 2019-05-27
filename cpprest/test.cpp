8c96bac5abca8f1269bc0d9f39851faabc6bfc4b
ddeka0 <dekadebashish3@gmail.com>
2019-05-27 17:17:39 +0530

    Create own task with functions

--- cpprest/test.cpp
+++ cpprest/test.cpp
@@ -21,57 +21,57 @@ using namespace web::http::client;
 
 /* some test code .......................... */
 enum {
-    REQUESTER_NF_TYPE   = 0,
-    TARGET_NF_TYPE,
-    SERVICE_NAMES,
-    SNSSAIS,
-    DNN,
+	REQUESTER_NF_TYPE   = 0,
+	TARGET_NF_TYPE,
+	SERVICE_NAMES,
+	SNSSAIS,
+	DNN,
 };
 
 map<std::string,int> queryIdMap = {
-    {"target-nf-type",          TARGET_NF_TYPE},
-    {"requester-nf-type",       REQUESTER_NF_TYPE},
-    {"service-names",           SERVICE_NAMES},
-    {"snssais",                 SNSSAIS},
-    {"dnn",                     DNN}
+	{"target-nf-type",          TARGET_NF_TYPE},
+	{"requester-nf-type",       REQUESTER_NF_TYPE},
+	{"service-names",           SERVICE_NAMES},
+	{"snssais",                 SNSSAIS},
+	{"dnn",                     DNN}
 };
 
 std::string getQueryString(std::map<std::string,json::value>& queryOptions) {
-    std::set<std::string> Set;
-    for(auto &e : queryOptions) {
-        switch(queryIdMap[e.first])
-        {
-            case REQUESTER_NF_TYPE:
-            case TARGET_NF_TYPE:
-            case DNN:
-            {
-                Set.insert(e.second.as_string());
-            }
-            break;
-            case SERVICE_NAMES:
-            {
-                for(auto &x: e.second.as_array()) {
-                    Set.insert(x.as_string());
-                }
-            }
-            break;
-            case SNSSAIS:
-            {
-                for(auto &x: e.second.as_array()) {
-                    Set.insert(x.at("sd").as_string());
-                    Set.insert(std::to_string(x.at("sst").as_number().to_int32()));
-                }
-            }
-            break;
-            default:
-                cerr << "Unknown query name type" << endl;
-        }
-    }
-    std::string res = "";
-    for(auto &e:Set) {
-    	res += e;
-    }
-    return res;
+	std::set<std::string> Set;
+	for(auto &e : queryOptions) {
+		switch(queryIdMap[e.first])
+		{
+			case REQUESTER_NF_TYPE:
+			case TARGET_NF_TYPE:
+			case DNN:
+			{
+				Set.insert(e.second.as_string());
+			}
+			break;
+			case SERVICE_NAMES:
+			{
+				for(auto &x: e.second.as_array()) {
+					Set.insert(x.as_string());
+				}
+			}
+			break;
+			case SNSSAIS:
+			{
+				for(auto &x: e.second.as_array()) {
+					Set.insert(x.at("sd").as_string());
+					Set.insert(std::to_string(x.at("sst").as_number().to_int32()));
+				}
+			}
+			break;
+			default:
+				cerr << "Unknown query name type" << endl;
+		}
+	}
+	std::string res = "";
+	for(auto &e:Set) {
+		res += e;
+	}
+	return res;
 }
 
 
@@ -117,74 +117,97 @@ int main() {
 
 	/*simulate server in the same file */
 
-    auto query =  uri::split_query(uri::decode(uri.query()));
-    /*  before processing any query ............................ 
-    *   for exmaple I want to get the query of requester-nf-type
-    *   I know it is a string, therefore parse it as string from val
-    *   similarly,
-    *   services is an array of strings ( from standard )
-    *   therefore first convert it to an array class and then convert it to
-    *   string using as_string()
-    * 
-    *   These loop need not run beforehand, access each query whenever required
-    *   and access accordingly
-    */
-    cout <<"printing .. queries provided by the map ... " << endl<<endl;    
-    for(auto & e : query) {
-        json::value val = json::value::parse(e.second);
-        if(e.first == "requester-nf-type" or e.first == "target-nf-type") {
-            std::string x = val.as_string();
-            cout <<e.first<<" "<<x << endl;
-        }else if(e.first == "snssais") {
-            int sst =  val.as_array()[0].at("sst").as_number().to_int32();
-            string sd = val.as_array()[0].at("sd").as_string();
+	auto query =  uri::split_query(uri::decode(uri.query()));
+	/*  before processing any query ............................ 
+	*   for exmaple I want to get the query of requester-nf-type
+	*   I know it is a string, therefore parse it as string from val
+	*   similarly,
+	*   services is an array of strings ( from standard )
+	*   therefore first convert it to an array class and then convert it to
+	*   string using as_string()
+	* 
+	*   These loop need not run beforehand, access each query whenever required
+	*   and access accordingly
+	*/
+	cout <<"printing .. queries provided by the map ... " << endl<<endl;    
+	for(auto & e : query) {
+		json::value val = json::value::parse(e.second);
+		if(e.first == "requester-nf-type" or e.first == "target-nf-type") {
+			std::string x = val.as_string();
+			cout <<e.first<<" "<<x << endl;
+		}else if(e.first == "snssais") {
+			int sst =  val.as_array()[0].at("sst").as_number().to_int32();
+			string sd = val.as_array()[0].at("sd").as_string();
 
-            cout << e.first <<" sd = "<< sd <<" sst = "<<sst<< endl;
-        }else if(e.first == "services") {
-            cout <<e.first<<" "<<val.as_array()[0].as_string() <<endl;
-        }
-    }
-    cout << endl;
-
-
+			cout << e.first <<" sd = "<< sd <<" sst = "<<sst<< endl;
+		}else if(e.first == "services") {
+			cout <<e.first<<" "<<val.as_array()[0].as_string() <<endl;
+		}
+	}
+	cout << endl;
 
 
-
-
-
-
-
-
-
-    /*someting extra ... not related to above */
+	/*someting extra ... not related to above */
 	cout <<"printing .. snssais and service-names ... " << endl<<endl;    
-    for(auto &e:nrfQueryOptions["snssais"].as_array()) {
-    	cout << e.at("sst").as_number().to_int32() <<" "<<e.at("sd").as_string() << endl;
-    }
+	for(auto &e:nrfQueryOptions["snssais"].as_array()) {
+		cout << e.at("sst").as_number().to_int32() <<" "<<e.at("sd").as_string() << endl;
+	}
 
-    for(auto &e:nrfQueryOptions["service-names"].as_array()) {
-    	cout << e.as_string() << endl;
-    }
-    cout << endl;
+	for(auto &e:nrfQueryOptions["service-names"].as_array()) {
+		cout << e.as_string() << endl;
+	}
+	cout << endl;
 
-    cout <<"printing .. total query string ... " << endl;
-    cout << getQueryString(nrfQueryOptions) << endl<<endl;
+	cout <<"printing .. total query string ... " << endl;
+	cout << getQueryString(nrfQueryOptions) << endl<<endl;
 
 
 
-    /* some other test ............................... */
-    cout <<"checking .. created hint is equal to query string or not ... " << endl<<endl;    
-    std::set<std::string> Set;
-    Set.insert(std::to_string(1)); // UE ID
-    Set.insert(std::to_string(1));
-    Set.insert("FFFFFF");
-    Set.insert("AMF");
-    Set.insert("SMF");
-    Set.insert("nudm-uecm");
-    Set.insert("test.com");
-    std::string res = "";
-    for(auto &e : Set) {
-    	res += e;
-    }
-    cout << ((res == getQueryString(nrfQueryOptions))?"query string matched":"query string does not match")<< endl;
+	/* some other test ............................... */
+	cout <<"checking .. created hint is equal to query string or not ... " << endl<<endl;    
+	std::multiset<std::string> Set;
+	Set.insert(std::to_string(1)); // UE ID
+	Set.insert(std::to_string(1));
+	Set.insert("FFFFFF");
+	Set.insert("AMF");
+	Set.insert("SMF");
+	Set.insert("nudm-uecm");
+	Set.insert("test.com");
+	std::string res = "";
+	for(auto &e : Set) {
+		res += e;
+	}
+	cout << ((res == getQueryString(nrfQueryOptions))?"query string matched":"query string does not match")<< endl;
+
+
+	json::value A;
+	json::value B;
+
+	B["x"] = json::value::string("debashish");
+
+	A["y"] = B;
+
+	if(A.has_object_field(U("y"))) {
+		cout << A.at(U("y")).at(U("x")).as_string() << endl;
+	}
+
+	cout <<"pplx thread test ... "<< endl<<endl;
+	
+	int cnt = 5;
+	pplx::task<std::string>([&cnt]()
+	{
+		sleep(1);
+		cout <<"Entry1 "<<std::this_thread::get_id()<< endl;
+		while(cnt >= 0) {
+			cout <<"loop .... "<<cnt--<<endl;
+		}
+		return std::string("Hello");
+	})
+	.then([](string x)
+	{	
+		cout <<"Entry2 "<<x<<" "<<std::this_thread::get_id()<<endl;
+	})
+	.wait();
+
+	cout <<"Exit from main "<<std::this_thread::get_id()<<endl;
 }
