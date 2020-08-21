72bcf964cdd50d9a64e70ca098cbb82a5f5f9f98
Debashish Deka <dekadebashish3@gmail.com>
2020-08-22 02:53:47 +0530

    std::thread polls token.stop_requested()

---
+++ concurrency/co-op-cancel_1.cpp
@@ -0,0 +1,33 @@
+#include <mutex>
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <stop_token>
+
+int main() {
+
+    std::stop_source stSrc;
+    auto token = stSrc.get_token();
+
+    std::thread t{[](int x,std::stop_token token) {
+        bool done = false;
+        while(!done) {
+            std::cout <<"["<<x<<"] Hello from inside of the std::thread "<<std::endl;
+            if(token.stop_requested()) {
+                std::cout <<"This thread work is done!" << std::endl;
+                done = true;
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+
+    },10,token};
+
+
+    // lets ask to stop the thread t
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    stSrc.request_stop();
+    std::cout <<"stop requested from main" << std::endl;
+    
+    
+    t.join();
+}
