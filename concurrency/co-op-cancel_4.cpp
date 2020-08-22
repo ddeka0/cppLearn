0d678b823efb5461cc9b2b502fa718f8584e863a
Debashish Deka <dekadebashish3@gmail.com>
2020-08-22 05:37:14 +0530

    concept is added to form a closure

--- concurrency/co-op-cancel_4.cpp
+++ concurrency/co-op-cancel_4.cpp
@@ -6,22 +6,25 @@
 #include <stop_token>
 #include <condition_variable>
 #include <future>
-#include <coroutine>
+#include <concepts>
+#include <atomic>
+
 
 #define Debug(arg) std::cout <<"Tid["<<std::this_thread::get_id()<<"]" \
     <<" "<<arg <<std::endl;
 
 
+void func(std::stop_token token,std::invocable<std::stop_token> auto&& f) {
+    f(token);
+}
+
 int main() {
     std::stop_source stSrc;
     auto token = stSrc.get_token();
-    
-
-    std::thread t{[](int x,std::stop_token token) {
+    std::jthread t{[](int x,std::stop_token token) {
         std::mutex m;
         std::condition_variable_any cv;
         std::unique_lock lock{m};
-
         Debug("Sleeping...");
         
         cv.wait_for(lock,token,std::chrono::seconds(10),[]{
@@ -31,12 +34,28 @@ int main() {
         Debug("Returning ...");
 
     },10,token};
+    
+    func(token,[](std::stop_token token) {
+        std::mutex m;
+        std::condition_variable_any cv;
+        std::unique_lock lock{m};
 
+        Debug("Sleeping...in invocable");
+        
+        cv.wait_for(lock,token,std::chrono::seconds(10),[]{
+            return false;
+        });
+        
+        Debug("Returning ...from invocable");
+    });
+    
     // lets ask to stop the thread t
     std::this_thread::sleep_for(std::chrono::seconds(2));
     stSrc.request_stop();
 
     Debug("Stop requested from main");
     
-    t.join();
+
+
+    
 }
