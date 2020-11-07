8bf97af79d617b042529edc267561b39c6dda5d8
Debashish Deka <dekadebashish3@gmail.com>
2020-11-08 03:05:49 +0530

    move semantics basic examples added

---
+++ moveSemantics/m3.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+#define mem(m)	(void*)(m)
+#define PrintF std::cout << __LINE__ <<" "<< __PRETTY_FUNCTION__ << std::endl
+#define PrintObjAddr	std::cout <<"This object : "<<mem(this) << std::endl;
+#define LineBreak  std::cout << std::endl << std::endl;
+
+void* operator new(size_t sz)
+{
+  void* m = malloc(sz);
+  std::cout<<"User Defined :: Operator new "<<sz <<" "<<mem(m) <<std::endl;
+  
+  return m;
+}
+// Overloading Global delete operator
+void operator delete(void* m)
+{
+  std::cout<<"User Defined :: Operator delete "<< mem(m) <<std::endl;
+  free(m);
+}
+// Overloading Global new[] operator
+void* operator new[](size_t sz)
+{
+  void* m = malloc(sz);
+  std::cout<<"User Defined :: Operator new [] "<<sz<<" "<< mem(m) <<std::endl;
+  return m;
+}
+// Overloading Global delete[] operator
+void operator delete[](void* m)
+{
+  std::cout<<"User Defined :: Operator delete[] "<<mem(m) <<std::endl;
+  free(m);
+}
+
+class Int {
+public:
+    Int() { PrintF; }
+    ~Int() { 
+		PrintF;
+		PrintObjAddr;
+		if(m_data != nullptr) {
+			std::cout<<__LINE__<<" "<<"deleted plain int "<< mem(m_data) << std::endl;
+			delete m_data;
+			m_data = nullptr;
+		}
+	}
+    Int(int x) {
+		PrintF;
+		PrintObjAddr;
+		if(m_data == nullptr) {
+			m_data = new int(x);
+			std::cout<<__LINE__<<" "<<"allocated plain int "<< mem(m_data) << std::endl;
+		}
+		else
+			*m_data = x;
+	}
+	
+	Int(const Int& other) {
+		// deep copy construction using other
+		PrintF;
+		PrintObjAddr;
+		m_data = new int;
+		std::cout<<__LINE__<<" "<<"allocated plain int "<< mem(m_data) << std::endl;
+		*m_data = *(other.m_data);
+	}
+
+	Int(Int&& other) noexcept {
+		// resource transfer from other 
+		PrintF;
+		PrintObjAddr;
+		m_data = other.m_data;
+		other.m_data = nullptr;
+	}
+
+	Int& operator=(const Int& other) {
+		// delete own resources and create new resource and copy other
+		PrintF;
+		if(this != &other) {
+			if(m_data != nullptr) {
+				std::cout<<__LINE__<<" " <<"deleted plain int "<< mem(m_data) << std::endl;
+				delete m_data;
+				m_data = nullptr;
+			}
+			m_data = new int;
+			std::cout<<__LINE__<<" "<<"allocated plain int "<< mem(m_data) << std::endl;
+			*m_data = *(other.m_data);
+		}
+		return *this;
+	}
+
+	Int& operator=(Int&& other) noexcept {
+		PrintF;
+		if(this != &other) {
+			if(m_data != nullptr) {
+				std::cout<<__LINE__<<" " <<"deleted plain int "<< mem(m_data) << std::endl;
+				delete m_data;
+				m_data = nullptr;
+			}
+			m_data = other.m_data;
+			other.m_data = nullptr;
+		}
+		return *this;
+	}
+
+    int* m_data = nullptr;
+};
+
+class Array {
+    public:
+    Array() {
+        PrintF;
+    }
+    ~Array() {
+        PrintF;
+		m_data.clear();
+    }
+
+	map<int,Int> m_data;
+};
+
+int main() {
+	std::cout << __LINE__ <<" => Op : "<< R"(Int a(1))" << std::endl;
+	Int a = 1;
+	
+	LineBreak;
+	std::cout << __LINE__ <<" => Op : "<< R"(Array arr)" << std::endl;
+	Array arr;
+	
+	LineBreak;
+	std::cout << __LINE__ <<" => Op : "<< R"(arr.m_data[0] = std::move(a);)" << std::endl;
+	arr.m_data[0] = std::move(a);
+	
+	LineBreak;
+	
+	std::cout << __LINE__ <<" => Op : "<< R"(Int b(2);)" << std::endl;
+	Int b(2);
+	
+	LineBreak;
+
+	std::cout << __LINE__ <<" => Op : "<< R"(arr.m_data[1] = std::move(b);)" << std::endl;
+	arr.m_data[1] = std::move(b);
+
+	LineBreak;
+
+	std::cout << __LINE__ <<" => Op : "<< R"(arr.m_data[0] = Int(10);)" << std::endl;
+	
+	arr.m_data[0] = Int(10);
+
+	LineBreak;
+
+	std::cout << __LINE__ <<" => Op : "<< R"(arr.m_data[0] = 10;)" << std::endl;
+	
+	arr.m_data[0] = 10;
+
+	LineBreak;
+}
